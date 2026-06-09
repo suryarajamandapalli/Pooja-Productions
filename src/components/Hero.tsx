f898d736e867d1480cd496851e52abbfe1cd560c
SURYA RAJA <YOUR_GITHUB_EMAIL>
2026-06-09 16:22:12 +0530

    Remove dark transparency overlay from hero video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,8 +39,6 @@ export const Hero: React.FC = () => {
               zIndex: 1
             }} 
           />
-          {/* Dark Transparency Overlay */}
-          <div style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, backgroundColor: "rgba(0, 0, 0, 0.6)", zIndex: 2 }} />
 
           {/* Diamonds moved to the next section */}
         </div>
