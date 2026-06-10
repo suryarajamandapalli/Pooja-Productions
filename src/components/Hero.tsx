6f2f02455c045a9c06eb591789209a6fae3a2679
SURYA RAJA <YOUR_GITHUB_EMAIL>
2026-06-10 06:43:13 +0530

    Revert cinematic scroll video feature and restore static image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Marquee } from "./Marquee";
 import { SplitText } from "./SplitText";
 import { useCMS } from "./CMSContext";
-import { CinematicVideoCard } from "./CinematicVideoCard";
 
 export const Hero: React.FC = () => {
   const { data } = useCMS();
@@ -100,7 +99,10 @@ export const Hero: React.FC = () => {
                 <div className="container-fluid p-0">
                   <div className="row g-0">
                     <div className="col-12">
-                      <CinematicVideoCard />
+                      <div
+                        className="divider divider-image main-image-1 animate-in-up"
+                        style={hero?.heroImageUrl ? { backgroundImage: `url(${hero.heroImageUrl})` } : undefined}
+                      />
                     </div>
                   </div>
                 </div>
