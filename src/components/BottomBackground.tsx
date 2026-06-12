0cd3601716be60dc709ade8458797e55abb7659e
SURYA RAJA <YOUR_GITHUB_EMAIL>
2026-06-12 09:25:08 +0530

    feat: remove bottom marquee and structured footers, leaving only one rotating gold diamond in right background

--- src/components/BottomBackground.tsx
+++ src/components/BottomBackground.tsx
@@ -3,12 +3,9 @@ import React from "react";
 export const BottomBackground: React.FC = () => {
   return (
     <div className="bottom__background bottom-bg-01" style={{ position: "absolute", zIndex: 1, pointerEvents: "none" }}>
-      {/* Original template bottom backgrounds using transparent PNG versions */}
-      <div className="bottom-bg-01__02 animate-card-2">
-        <img src="/img/backgrounds/gold_diamond1.png" alt="Template background image" className="slow-rotate-left" />
-      </div>
+      {/* Only one rotating gold diamond on the right forming a rotating semicircle */}
       <div className="bottom-bg-01__01 animate-card-2">
-        <img src="/img/backgrounds/gold_diamond2.png" alt="Template background image" className="slow-rotate-right" />
+        <img src="/img/backgrounds/gold_diamond1.png" alt="Template background image" className="slow-rotate-right" />
       </div>
     </div>
   );
